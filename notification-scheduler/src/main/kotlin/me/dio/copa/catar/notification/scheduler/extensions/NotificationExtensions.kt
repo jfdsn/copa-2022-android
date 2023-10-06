ac9af6b072b3b34118839cc6d2295c2e52212dd4
Jonathan Faria <jonathanfdsn@gmail.com>
2023-10-06 16:58:13 -0300

    Changes in NotificationExtensions.kt

--- notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationExtensions.kt
+++ notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationExtensions.kt
@@ -1,10 +1,17 @@
 package me.dio.copa.catar.notification.scheduler.extensions
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import me.dio.copa.catar.notification.scheduler.R
@@ -16,6 +23,16 @@ private const val NOTIFICATION_INTENT_REQUEST_CODE = 0
 fun Context.showNotification(title: String, content: String) {
     createNotificationChannel()
     val notification = getNotification(title, content)
+
+    //Verifica se o usuario não aceita notificações e o informa da necessidade do mesmo
+    if (ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        requestNotificationPermission()
+        return
+    }
 
     NotificationManagerCompat
         .from(this)
@@ -49,3 +66,29 @@ private fun Context.getOpenAppPendingIntent() = PendingIntent.getActivity(
     packageManager.getLaunchIntentForPackage(packageName),
     PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
 )
+
+
+private fun Context.requestNotificationPermission() {
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle("Permissão de Notificação")
+    builder.setMessage("Para receber notificações, é necessário conceder permissão de notificação.")
+    builder.setPositiveButton("OK") { _, _ ->
+        openAppSettings()
+    }
+    builder.setNegativeButton("Cancelar") { dialog, _ ->
+        dialog.dismiss()
+    }
+    builder.show()
+}
+
+// Redireciona o usuário para as configurações do aplicativo
+private fun Context.openAppSettings() {
+    val intent = Intent()
+    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+    intent.addCategory(Intent.CATEGORY_DEFAULT)
+    intent.data = Uri.parse("package:$packageName")
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+    intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+    startActivity(intent)
+}
